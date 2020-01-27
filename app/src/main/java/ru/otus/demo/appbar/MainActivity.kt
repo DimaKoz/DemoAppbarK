480f50ee3ea168a4000217b0ef5d1d8bd2d6af73
kozhevin <kozhevin.dima@gmail.com>
2020-01-27 09:20:38 +0300

    Init

---
+++ app/src/main/java/ru/otus/demo/appbar/MainActivity.kt
@@ -0,0 +1,35 @@
+package ru.otus.demo.appbar
+
+import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        val actionBar = supportActionBar
+        actionBar?.setDisplayHomeAsUpEnabled(true)
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu): Boolean {
+        val inflater = menuInflater
+        inflater.inflate(R.menu.menu_main, menu)
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        val id = item.itemId
+        if (id == R.id.action_settings) {
+            Toast.makeText(this, "Menu Item action_settings selected", Toast.LENGTH_SHORT).show()
+            return true
+        }
+        if (id == android.R.id.home) {
+            onBackPressed()
+            return true
+        }
+        return super.onOptionsItemSelected(item)
+    }
+}
